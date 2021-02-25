55615696aa25f82878ede0d53c902b2ef9eda065
Uğurhan Uslu <ugurhanuslu@gmail.com>
2021-02-26 01:25:13 +0300

    ugurhanuslu
    Final.

--- src/main/java/Model/Variables.java
+++ src/main/java/Model/Variables.java
@@ -5,7 +5,7 @@ public class Variables {
     public static String email = "ornek@mail.com.tr";
     public static String userPassword = "Testinium2021";
     public static String baseUrl = "https://www.gittigidiyor.com/";
-    public static String log4jPath = systemPath +"\\src\\test\\java\\log4j.properties";
-    public static String chromePath = systemPath + "\\drivers\\chromedriver.exe";
-    public static String firefoxPath = systemPath + "\\drivers\\geckodriver.exe";
+    public static String log4jPath = "src/test/java/log4j.properties";
+    public static String chromePath = "drivers/chromedriver.exe";
+    public static String firefoxPath = "drivers/geckodriver.exe";
 }
