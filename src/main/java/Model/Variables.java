79f2d2f2318e5646258f66dddcc386d729937ec5
Uğurhan Uslu <ugurhanuslu@gmail.com>
2021-02-26 01:15:09 +0300

    ugurhanuslu
    absolute path added

--- src/main/java/Model/Variables.java
+++ src/main/java/Model/Variables.java
@@ -1,10 +1,11 @@
 package Model;
 
 public class Variables {
+    public static String systemPath = System.getProperty("user.dir");
     public static String email = "ornek@mail.com.tr";
     public static String userPassword = "Testinium2021";
     public static String baseUrl = "https://www.gittigidiyor.com/";
-    public static String log4jPath = "C:\\Users\\Uğurhan Uslu\\IdeaProjects\\testinium\\src\\test\\java\\log4j.properties";
-    public static String chromePath = "C:\\Users\\Uğurhan Uslu\\IdeaProjects\\testinium\\drivers\\chromedriver.exe";
-    public static String firefoxPath = "C:\\Users\\Uğurhan Uslu\\IdeaProjects\\testinium\\drivers\\geckodriver.exe";
+    public static String log4jPath = systemPath +"\\src\\test\\java\\log4j.properties";
+    public static String chromePath = systemPath + "\\drivers\\chromedriver.exe";
+    public static String firefoxPath = systemPath + "\\drivers\\geckodriver.exe";
 }
